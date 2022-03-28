ea4b9351a455a8130c34a0e010bf9019b4d37888
Samuel <slemm@stu.vistula.edu.pl>
2022-03-29 01:03:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        int arr[][] = new int[0][];
+            int n = 0;
+            int j= 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++ ){
+            sum += arr[i][j];
+            
+        }
+        System.out.println("sum :" + sum);
+        }
+
+
+
+    }
+
+
+
+
